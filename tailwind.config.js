499de354fb9baf299a9672e2605ebb32459bcb84
alysialfi <9c.alysialfi@gmail.com>
2024-03-10 02:54:08 +0700

    feat(tailwind.config): adjust animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+      },
+			animation: {
+        'waving-envelope': 'wave 4s linear infinite',
+				'pulse': 'pulse 5s cubic-bezier(0.6, 0, 0.8, 1) infinite'
+      },
+		}
   },
   plugins: [],
 }
